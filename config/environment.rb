2009edd1ce9a1f80e5e6b7b7a80481bfe0c4ca5b
noramm-12 <a0960186130@gmail.com>
2024-03-27 17:35:46 +0800

    claen up layouts

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
-#To remove the field_with_errors functionality
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+# To remove the field_with_errors functionality
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
   html_tag.html_safe
 end
