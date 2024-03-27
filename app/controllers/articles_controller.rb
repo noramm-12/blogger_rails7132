72e6ca1a4c1b499b242febb40165e34261d01448
noramm-12 <a0960186130@gmail.com>
2024-03-27 17:13:50 +0800

    style show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,40 +2,36 @@
 
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
-  def show
-    # byebug
-  end
-
   def index
     @articles = Article.all
+  end
+  def show
   end
 
   def new
     @article = Article.new
   end
 
-  def edit; end
-
   def create
-    #render plain: params[:article]
-    binding.break
+    # render plain: params[:article]
+
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was created successfully.' # sending success message by flash
       redirect_to article_path(@article) # prefix:article => show.html.erb
     else
-      render 'new' # new.html erb
+      render 'new', status: :unprocessable_entity# new.html erb
     end
 
     # render plain: @article.inspect
   end
-
+  def edit; end
   def update
     if @article.update(article_params)
       flash[:notice] = 'Article was edited successfully.' # sending success message by flash
       redirect_to article_path(@article) # show
     else
-      render 'edit'
+      render 'edit', status: :unprocessable_entity
     end
   end
 
