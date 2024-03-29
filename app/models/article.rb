004fa5067b1351b6799926f246895a29150ee809
noramm-12 <a0960186130@gmail.com>
2024-03-29 14:37:45 +0800

    [fix/backend] add one-to-many association between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 1, maximum:10 }
   validates :description, presence: true, length: { minimum: 1, maximum: 100 }
 end
