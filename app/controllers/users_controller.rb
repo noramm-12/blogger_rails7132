c8d9662b1a67c5a52016694b2fcd4632bb47d1d3
noramm-12 <a0960186130@gmail.com>
2024-04-03 14:13:23 +0800

    dry user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user,only:[:show, :edit,:update]
   def show
-    @user = User.find(params[:id])
     @articles=@user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id]=@user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -23,11 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your information was successfully updated.'
       redirect_to @user
@@ -41,5 +40,8 @@ class UsersController < ApplicationController
   def user_params
     # params[:user]
     params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
