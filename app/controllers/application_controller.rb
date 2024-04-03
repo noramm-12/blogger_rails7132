9b8597da06488266a075982670a5eebdc38c74dc
noramm-12 <a0960186130@gmail.com>
2024-04-04 00:47:32 +0800

    controller methods as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+  # 取得當前用戶
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-    helper_method :current_user
-    #取得當前用戶
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+  # 是否登入
+  def logged_in?
+    !!current_user
+  end
 end
