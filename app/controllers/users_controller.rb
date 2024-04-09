ea9e326763ac586a1ed182123eb69b8cc291e727
noramm-12 <a0960186130@gmail.com>
2024-04-10 00:16:31 +0800

    require login and same user restrictions from articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user,only:[:show, :edit,:update]
+  before_action :require_user, except: %i[index show]
   def show
     @articles=@user.articles.paginate(page: params[:page], per_page: 5)
   end
