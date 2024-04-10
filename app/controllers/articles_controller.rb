d469c96c218d4d7a0654bb0be0c6d4bbcccaf838
noramm-12 <a0960186130@gmail.com>
2024-04-10 12:43:45 +0800

    require login and same user restrictions from users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
   before_action :require_user, except: %i[index show]
-  before_action :require_same_user, only:%i[edit,update,destroy]
+  before_action :require_same_user, only: %i[edit update destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -55,9 +55,9 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def require_same_user
-    if current_user!=@article.user
+    if current_user!= @article.user
       flash[:alert]="You can only edit or delete your own article"
-      redirect_to_@article
+      redirect_to @article
     end
   end
 end
