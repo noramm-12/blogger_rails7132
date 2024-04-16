4d283e526b27f4f8299023a6c4d0ab804791f058
noramm-12 <a0960186130@gmail.com>
2024-04-17 00:55:25 +0800

    integration test:create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,36 @@
 class CategoriesController < ApplicationController
-
-  def new
-
-  end
-
   def index
 
   end
 
   def show
+    @category=Category.find(params[:id])
+  end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  # def edit
+  #
+  # end
+  # def update
+  #
+  # end
+
+  private
+  def category_params
+    # params[:article]
+    params.require(:category).permit(:name)
   end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,28 +1,31 @@
-require "test_helper"
+require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     # @category = categories(:one)
-    @category = Category.create(name: "Sports")
+    @category = Category.create(name: 'Sports')
   end
 
-  test "should get index" do
+  test 'should get index' do
     get categories_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_category_url
     assert_response :success
   end
-  #
-  # test "should create category" do
-  #   assert_difference("Category.count") do
-  #     post categories_url, params: { category: {  }
-  #   assert_redirected_to category_url(Category.last) notice
-  # end
 
-  test "should show category" do
+  test 'should create category' do
+    #確保在執行指定區塊之後，Category 模型的數量增加了 1
+    assert_difference('Category.count', 1) do
+      post categories_url, params: { category: { name: 'Travel' } }
+    end
+    #確保該請求被成功重定向到了最後創建的類別的 URL
+    assert_redirected_to category_url(Category.last)
+  end
+
+  test 'should show category' do
     get category_url(@category)
     assert_response :success
   end
