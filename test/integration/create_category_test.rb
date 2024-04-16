21f575eec28de3b34ca0c1bb32c9d31606726996
noramm-12 <a0960186130@gmail.com>
2024-04-17 01:16:20 +0800

    Create integration test for invalid category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -11,5 +11,15 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match "Sports",response.body #確保響應體中包含新創建的類別名稱
+  end
+
+  test "get new category form and reject invalid category submissions" do
+    get "/categories/new"
+    assert_response :success
+    assert_difference('Category.count',0) do
+      post categories_url, params: { category: { name: ' ' } }
     end
+    assert_match "errors",response.body #確保響應體中包含新創建的類別名稱
+    assert_select "div.alert"
+  end
 end
